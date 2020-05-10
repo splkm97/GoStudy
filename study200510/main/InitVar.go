fc87d37d285395bab453babf4b67046c9e4bb294
splkm97 <50478930+splkm97@users.noreply.github.com>
2020-05-10 12:28:16 +0900

    Update InitVar.go

--- study200510/main/InitVar.go
+++ study200510/main/InitVar.go
@@ -15,7 +15,17 @@ func main() {
     fmt.Println("Type of c: ", reflect.TypeOf(c))
     fmt.Println("Type of python: ", reflect.TypeOf(python))
     fmt.Println("Type of java: ", reflect.TypeOf(java))
+    
+    fmt.Println("Value of x: ", x)
+    fmt.Println("Value of y: ", y)
+    fmt.Println("Value of z: ", z)
+    fmt.Println("Value of c: ", c)
+    fmt.Println("Value of python: ", python)
+    fmt.Println("Value of java: ", java)
+    
     // 실행 결과 x, y, z, c, python, java 는 각각
-    // int, int, int, bool, int32, string이었다.
+    // int, int, int, bool, int32, string 타입이었고,
+    // 1, 2, 3, true, 70, no! 값을 가졌다.
     // char -> int32인건가?
+    // 출력은 'F'의 아스키 값인 70인걸로 보아 char타입 자체가 없어보임..
 }
