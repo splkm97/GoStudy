491121ab6af2ef9a679ba0443fc8b632a3239989
splkm97 <50478930+splkm97@users.noreply.github.com>
2020-05-10 16:13:26 +0900

    Create ShortVar.go

---
+++ study200510/main/ShortVar.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+    var x, y, z int = 1, 2, 3
+    // 다음과 같이 변수 선언 및 초기화를 수행할 수 있다.
+    c, python, java := true, false, "no!"
+
+    fmt.Println(x, y, z, c, python, java)
+}
